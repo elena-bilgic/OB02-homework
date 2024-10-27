34c6b74652e95bb1c241d32ee155a420be30c717
elena-bilgic <elena.lipatnikova777@gmail.com>
2024-10-28 00:12:03 +0300

    str method added to User class
    __str__ method was added to User class to ensure more user-friendly representation of User list changes in Prints

--- main.py
+++ main.py
@@ -16,6 +16,11 @@ class User:
     def set_name(self, name):
         self._name = name
 
+    # method added to User class to provide a meaningful string representation of User objects
+    def __str__(self):
+        return f"User ID: {self._user_id}, Name: {self._name}, Access Level: {self._access_level}"
+
+
 class Admin(User):
     def __init__(self, user_id, name):
         super().__init__(user_id, name)
@@ -24,12 +29,12 @@ class Admin(User):
     def add_user(self, user_list, user):
         user_list.append(user)
         print(f"User {user.get_name()} added to the list.")
-        print(user_list)
+        print([str(u) for u in user_list])
 
     def remove_user(self, user_list, user):
         if user in user_list:
             user_list.remove(user)
-            print(user_list)
+            print([str(u) for u in user_list])
         else:
             print(f"User {user.get_name()} not found in the list.")
 
@@ -37,8 +42,9 @@ users = []
 admin1 = Admin("a1", "Steve")
 user1 = User("u1", "Peter")
 user2 = User("u2", "Paris")
-
+user3 = User("u3", "John")
 
 admin1.add_user(users,user1)
 admin1.remove_user(users,user2)
-print(users)
+admin1.add_user(users,user3)
+print([str(u) for u in users])
